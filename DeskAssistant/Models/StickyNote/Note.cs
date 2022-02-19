d922ab8eae027b3b504036962fd4f6d3addbc786
lzlocki <lzlocki@poczta.fm>
2022-02-19 08:47:49 +0100

    Set up project

---
+++ DeskAssistant/Models/StickyNote/Note.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeskAssistant.Models.StickyNote
+{
+    internal class Note
+    {
+    }
+}
