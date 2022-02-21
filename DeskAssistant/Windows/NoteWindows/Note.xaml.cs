54d5bb288e1dbc821f73fc46cb3bf18e0ce8fc41
lzlocki <lzlocki@poczta.fm>
2022-02-21 21:25:58 +0100

    Add references after remodeling of project

--- DeskAssistant/Windows/NoteWindows/Note.xaml.cs
+++ DeskAssistant/Windows/NoteWindows/Note.xaml.cs
@@ -1,18 +1,8 @@
 ﻿using DeskAssistant.Models.StickyNote;
 using DeskAssistant.Services.Note_Service;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace DeskAssistant.Windows.NoteWindows
 {
